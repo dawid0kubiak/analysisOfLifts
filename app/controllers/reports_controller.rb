f812def078516e48995f639427d600d348d3ef42
Dawid Kubiak <dawid0kubiak@gmail.com>
2018-01-12 00:35:30 +0100

    filters

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,39 +4,48 @@ class ReportsController < ApplicationController
 
   def cards
     @lifts = @lifts.where(transaction_type: 'Płatność kartą')
-
-    @name_data = @lifts.group(:name).select(:name, :amount).sum(:amount)
-                     .map {|data| {value: data[1].abs, label: data[0]}}.to_json
-
-    @type_data = @lifts.group(:lift_type_id).select(:lift_type_id, :amount).sum(:amount)
-                     .map {|data| {value: data[1].abs, label: LiftType.find(data[0]).name}}.to_json
+    @chart_data = chart_data @lifts
   end
 
   def all_operation
-    @sql = ''
-    if params.keys.include? 'simple'
-      simples = params[:simple]
-      simples.each do |simple|
-        @sql = where_build simple, @sql
-      end
-    elsif params.keys.include? 'product'
-      @sql = LiftType.find(params[:product]).condition
-    elsif params.keys.include? 'value'
-      @sql = params[:value]
-    end
+    @sql = sql_build
     @lifts = @lifts.where('amount < 0').where(@sql)
+    @chart_data = chart_data @lifts
 
-    @name_data = @lifts.group(:name).select(:name, :amount).sum(:amount)
-                     .map {|data| {value: data[1].abs, label: data[0]}}.to_json
-
-    @type_data = @lifts.group(:lift_type_id).select(:lift_type_id, :amount).sum(:amount)
-                     .map {|data| {value: data[1].abs, label: LiftType.find(data[0]).name}}.to_json
   end
 
   private
 
+  def chart_data(lifts)
+    name_data = lifts.group(:name).select(:name, :amount).sum(:amount)
+                    .map {|data| {
+                        value: data[1].abs, label: data[0]}}.to_json
+
+    data = lifts.group(:name).select(:name, :amount).sum(:amount)
+    sum = data.map{|d| d[1]}.sum
+
+    type_data = lifts.group(:lift_type_id).select(:lift_type_id, :amount).sum(:amount)
+                    .map {|data| {
+                        value: data[1].abs, label: "% ".html_safe+LiftType.find(data[0]).name}}.to_json
+    {size: {height: 300, width: 500}, name: name_data, type: type_data}
+  end
+
+  def sql_build
+    sql = ''
+    if params.keys.include? 'simple'
+      simples = params[:simple]
+      simples.each do |simple|
+        sql = where_build simple, sql
+      end
+    elsif params.keys.include? 'product'
+      sql = LiftType.find(params[:product]).condition
+    elsif params.keys.include? 'value'
+      sql = params[:value]
+    end
+    sql
+  end
+
   def init
-    @pie_size = {height: 400, width: 600}
     @lifts = Lift.where('user_id = ? and lift_type_id <> -2', current_user.id).order(date_of_commissioned: :desc)
   end
 

afcff5b17b4587a86a0461966be60c88f3bb63b0
Dawid Kubiak <dawid.kubiak@i-bs.pl>
2018-01-11 15:56:24 +0100

    filters

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -6,10 +6,10 @@ class ReportsController < ApplicationController
     @lifts = @lifts.where(transaction_type: 'Płatność kartą')
 
     @name_data = @lifts.group(:name).select(:name, :amount).sum(:amount)
-                      .map { |data| { value: data[1].abs, label: data[0] } }.to_json
+                     .map {|data| {value: data[1].abs, label: data[0]}}.to_json
 
     @type_data = @lifts.group(:lift_type_id).select(:lift_type_id, :amount).sum(:amount)
-                      .map { |data| { value: data[1].abs, label: LiftType.find(data[0]).name } }.to_json
+                     .map {|data| {value: data[1].abs, label: LiftType.find(data[0]).name}}.to_json
   end
 
   def all_operation
@@ -19,24 +19,24 @@ class ReportsController < ApplicationController
       simples.each do |simple|
         sql = where_build simple, sql
       end
-    elsif params.keys.include? 'saved'
-
-    elsif params.keys.include? 'advanced'
-
+    elsif params.keys.include? 'product'
+      sql = LiftType.find(params[:product]).condition
+    elsif params.keys.include? 'value'
+      sql = params[:value]
     end
     @lifts = @lifts.where('amount < 0').where(sql)
 
     @name_data = @lifts.group(:name).select(:name, :amount).sum(:amount)
-                      .map { |data| { value: data[1].abs, label: data[0] } }.to_json
+                     .map {|data| {value: data[1].abs, label: data[0]}}.to_json
 
     @type_data = @lifts.group(:lift_type_id).select(:lift_type_id, :amount).sum(:amount)
-                      .map { |data| { value: data[1].abs, label: LiftType.find(data[0]).name } }.to_json
+                     .map {|data| {value: data[1].abs, label: LiftType.find(data[0]).name}}.to_json
   end
 
   private
 
   def init
-    @pie_size = {height: 400, width: 600 }
+    @pie_size = {height: 400, width: 600}
     @lifts = Lift.where('user_id = ? and lift_type_id <> -2', current_user.id).order(date_of_commissioned: :desc)
   end
 
@@ -47,10 +47,10 @@ class ReportsController < ApplicationController
     operator = simple[:operator]
 
     sql += case condition
-           when /like/
-             filed + ' ' + condition + ' "%' + value + '%" ' + add_oper(operator)
-           else
-             filed + ' ' + condition + ' "' + value + '" ' + add_oper(operator)
+             when /like/
+               filed + ' ' + condition + ' "%' + value + '%" ' + add_oper(operator)
+             else
+               filed + ' ' + condition + ' "' + value + '" ' + add_oper(operator)
            end
     sql
   end
